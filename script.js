00a3f88de4b9da8d8e2078119c7ebded560eee3a
Ayush Singh <akhileshayush.2016@gmail.com>
2023-06-24 12:52:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 function threeSum(arr, target) {
-// write your code here
-  
+let triplets = [];
+
+  // Sort the array in ascending order
+  arr.sort((a, b) => a - b);
+
+  const n = arr.length;
+
+  for (let i = 0; i < n - 2; i++) {
+    // Skip duplicates
+    if (i > 0 && arr[i] === arr[i - 1]) {
+      continue;
+    }
+
+    let left = i + 1;
+    let right = n - 1;
+
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+
+      if (sum === target) {
+        triplets.push([arr[i], arr[left], arr[right]]);
+
+        // Skip duplicates
+        while (left < right && arr[left] === arr[left + 1]) {
+          left++;
+        }
+        while (left < right && arr[right] === arr[right - 1]) {
+          right--;
+        }
+
+        // Move the pointers
+        left++;
+        right--;
+      } else if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+	 return triplets;
 }
 
 module.exports = threeSum;
